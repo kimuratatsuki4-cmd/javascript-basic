172e157b8e8dad3d683505cf6bccb30fffff6192
kimuratatsuki4-cmd <kimuratatsuki4@gmail.com>
2025-08-08 21:06:54 +0900

    作成分をコミット

---
+++ js/variable.js
@@ -0,0 +1,15 @@
+// 変数の宣言
+let userName;
+userName = 'Tatsuki';
+let userAge = 25;
+
+console.log(userName);
+console.log(userAge);
+
+userName = 'Tatsuki Tanaka';
+userAge = 26;
+const userCountry = 'Japan';
+
+console.log(userName);
+console.log(userAge);
+console.log(userCountry);
